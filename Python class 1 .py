2f0957ae2b4e71c376a51f1e05c21993c3280c69
milonnub <milon.nub1@gmail.com>
2026-02-17 02:03:30 +0600

    first commit

---
+++ Python class 1 .py
@@ -0,0 +1,134 @@
+print("My Name is Milon Miya","and this is my first class")
+
+#Variable and value
+Name="Milon Miya"
+Age=27
+Result=3.14
+Old=False
+All_informations=True
+print(Name)
+print(Age)
+print(Result)
+print(All_informations)
+print(Old)
+
+#Others Way:
+print("Name:",Name) 
+print("Age:",Age)
+print("Result:",Result)
+print("Information:",All_informations)
+print("age:",Old)
+
+
+#Data Type
+print(type(Name))
+print(type(Age))
+print(type(Result))
+print(type(All_informations))
+print(type(Old))
+
+
+#print Sum
+
+a=10
+b=3
+sum=a+b
+print(sum)
+a=50
+b=30
+sum=a-b
+print(sum)
+
+#Comment in python
+#single line comment start with #
+#Multi-line comment:
+print("Hello world")
+print("Hello world")
+print("Hello world")
+
+#Operator:
+# Arithmetic Operator:
+a = 10
+b = 3
+print(a + b)
+print(a % b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#relational operators
+a =50
+b =20
+print(a == b) #false
+print(a != b) #true
+print(a >= b) #True
+print(a > b) #True
+print(a<b) #False
+
+#Assignment Operator:
+num =10
+num +=5
+print(num)
+
+
+
+#Logical Operator >not
+a = 10
+b = 5
+print (not(a>b))
+print (not(a<b))
+value1=False
+value2=False
+print(value1 and value2)
+val1=True
+val2=False
+print(value1 or value2)
+
+
+#ype Conversion:
+#1.Implicit type conversion python auto can understant
+
+a=2
+b=4.25
+print(type(a))
+print(type(b))
+print(a)
+print(b)
+
+#2.Explicit type Conversion Manually do programmer
+a=int ("2")
+b= float("4.25")
+print(type(a))
+print(type(b))
+print(a)
+print(b)
+
+a=3.14
+a=str(a)
+print(type(a))
+
+name=input ("enter your name:")
+print("welcome",name)
+ 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
