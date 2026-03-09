a8a4150c2532165bbe37ea0c031aeeebd8ed3946
milonnub <milon.nub1@gmail.com>
2026-03-09 22:12:08 +0600

    new code

---
+++ python class 4.py
@@ -0,0 +1,16 @@
+"""print("My name is: Milon Miya","i am\na boy")
+name="Milon Miya"
+print("My name is:",name)
+print(type(name))
+
+name = input("Enter your name:")
+print("Your name is:", name)
+
+a=20
+b=10
+sum=a+b
+print (sum)"""
+
+print("Hello World")
+name="Maruf Miya"
+print ("My name is:", name)
